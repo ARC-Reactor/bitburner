0fc93d431915d41351a5bf3952108fc2c3d769d4
ARC-Reactor <39143732+ARC-Reactor@users.noreply.github.com>
2024-02-22 13:01:17 -0500

    Update basic-server.js

--- basic-server.js
+++ basic-server.js
@@ -1,52 +1,91 @@
 /** @param {NS} ns */
 export async function main(ns) {
     const targets = [
-      "n00dles",
-      "nectar-net",
-      "max-hardware",
-      "neo-net",
-      "omega-net",
-      "foodnstuff",
-      "sigma-cosmetics",
-      "joesguns",
-      "hong-fang-tea",
-      "harakiri-sushi",
-      "CSEC",
-      "phantasy",
-      "netlink",
-      "zb-institute",
-      "crush-fitness",
-      "I.I.I.I",
-      "avmnite-02h",
-      "syscore",
-      "iron-gym",
-      "zer0",
-      "silver-helix",
-      "the-hub",
-      "computek",
-      "johnson-ortho",
-      "rothman-uni",
-      "summit-uni",
-      "catalyst",
+        "n00dles",
+        "zer0",
+        "nectar-net",
+        "omega-net",
+        "the-hub",
+        "catalyst",
+        "netlink",
+        "zb-institute",
+        "summit-uni",
+        "I.I.I.I",
+        "rho-construction",
+        "snap-fitness",
+        "alpha-ent",
+        "global-pharm",
+        "aevum-police",
+        "CSEC",
+        "foodnstuff",
+        "sigma-cosmetics",
+        "joesguns",
+        "hong-fang-tea",
+        "harakiri-sushi",
+        "max-hardware",
+        "neo-net",
+        "avmnite-02h",
+        "silver-helix",
+        "computek",
+        "johnson-ortho",
+        "rothman-uni",
+        "lexo-corp",
+        "syscore",
+        "millenium-fitness",
+        "galactic-cyber",
+        "unitalife",
+        "zeus-med",
+        "aerocorp",
+        "omnia",
+        "icarus",
+        "zb-def",
+        "univ-energy",
+        "infocomm",
+        "nova-med",
+        "deltaone",
+        "defcomm",
+        "taiyang-digital",
+        "solaris",
+        "crush-fitness",
+        "phantasy",
+        "iron-gym",
+        "microdyne",
+        "fulcrumtech",
+        "run4theh111z",
+        "clarkinc",
+        "megacorp",
+        "ecorp",
+        "blade",
+        "b-and-a",
+        "kuai-gong",
+        "nwo",
+        "applied-energetics",
+        "helios",
+        "vitalife",
+        "omnitek",
+        ".",
+        "titan-labs",
+        "stormtech",
+        "4sigma",
+        "powerhouse-fitness",
+        "fulcrumassets",
+        "The-Cave",
       ]
 
       while(true) {
           for (let target of targets){
             if  (ns.hasRootAccess(target) == true){
               let moneyThresh = ns.getServerMaxMoney(target);
-              let securityThresh = ns.getServerMinSecurityLevel(target);
-              if (ns.getServerSecurityLevel(target) > securityThresh) {
-            // If the server's security level is above our threshold, weaken it
-                  await ns.weaken(target);
-              } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-            // If the server's money is less than our threshold, grow it
-                  await ns.grow(target);
-              } else {
-            // Otherwise, hack it
-                  await ns.hack(target);
+              if (moneyThresh != 0){
+                let securityThresh = ns.getServerMinSecurityLevel(target);
+                while (ns.getServerMoneyAvailable(target) < moneyThresh) {
+                    await ns.grow(target);
+                } while (ns.getServerSecurityLevel(target) > securityThresh) {
+                    await ns.weaken(target);
+                } 
+                await ns.hack(target);
               }
             }
-            //else{getRoot(target);}
             }
           }
 }
