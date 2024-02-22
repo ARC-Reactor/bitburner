37303a22522a110b41b6a1cf9989e498618acee8
ARC-Reactor <39143732+ARC-Reactor@users.noreply.github.com>
2024-02-22 12:49:23 -0500

    Update purchase-server.js

--- purchase-server.js
+++ purchase-server.js
@@ -2,14 +2,14 @@
 export async function main(ns) {
     // How much RAM each purchased server will have. In this case, it'll
     // be 8GB.
-    const ram = 32;
+    const ram = 32768;
 
     // Iterator we'll use for our loop
     let i = 0;
 
     // Continuously try to purchase servers until we've reached the maximum
     // amount of servers
-    while (i < ns.getPurchasedServerLimit()) {
+    while (i < 5) {
         // Check if we have enough money to purchase a server
         if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
             // If we have enough money, then:
@@ -17,13 +17,96 @@ export async function main(ns) {
             //  2. Copy our hacking script onto the newly-purchased server
             //  3. Run our hacking script on the newly-purchased server with 3 threads
             //  4. Increment our iterator to indicate that we've bought a new server
+            let scriptname = "basic-server-0.js"
             let hostname = ns.purchaseServer("pserv-" + i, ram);
-            ns.scp("basic-server.js", hostname);
-            ns.exec("basic-server.js", hostname, 12);
+            ns.scp(scriptname, hostname);
+            let hostRam = ns.getServerMaxRam(hostname);
+            let ram = Math.trunc(hostRam / ns.getScriptRam(scriptname, hostname));
+            ns.exec(scriptname, hostname, ram);
             ++i;
+            await ns.sleep(60000);
         }
-        //Make the script wait for a second before looping again.
-        //Removing this line will cause an infinite loop and crash the game.
-        await ns.sleep(1000);
     }
+    
+        await ns.sleep(60000);
+    while (i < 10) {
+        // Check if we have enough money to purchase a server
+        if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
+            // If we have enough money, then:
+            //  1. Purchase the server
+            //  2. Copy our hacking script onto the newly-purchased server
+            //  3. Run our hacking script on the newly-purchased server with 3 threads
+            //  4. Increment our iterator to indicate that we've bought a new server
+            let scriptname = "basic-server-1.js"
+            let hostname = ns.purchaseServer("pserv-" + i, ram);
+            ns.scp(scriptname, hostname);
+            let hostRam = ns.getServerMaxRam(hostname);
+            let ram = Math.trunc(hostRam / ns.getScriptRam(scriptname, hostname));
+            ns.exec(scriptname, hostname, ram);
+            ++i;
+            await ns.sleep(60000);
+        }
+    }
+    
+        await ns.sleep(60000);
+    while (i < 15) {
+        // Check if we have enough money to purchase a server
+        if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
+            // If we have enough money, then:
+            //  1. Purchase the server
+            //  2. Copy our hacking script onto the newly-purchased server
+            //  3. Run our hacking script on the newly-purchased server with 3 threads
+            //  4. Increment our iterator to indicate that we've bought a new server
+            let scriptname = "basic-server-2.js"
+            let hostname = ns.purchaseServer("pserv-" + i, ram);
+            ns.scp(scriptname, hostname);
+            let hostRam = ns.getServerMaxRam(hostname);
+            let ram = Math.trunc(hostRam / ns.getScriptRam(scriptname, hostname));
+            ns.exec(scriptname, hostname, ram);
+            ++i;
+            await ns.sleep(60000);
+        }
+    }
+    
+        await ns.sleep(60000);
+    while (i < 20) {
+        // Check if we have enough money to purchase a server
+        if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
+            // If we have enough money, then:
+            //  1. Purchase the server
+            //  2. Copy our hacking script onto the newly-purchased server
+            //  3. Run our hacking script on the newly-purchased server with 3 threads
+            //  4. Increment our iterator to indicate that we've bought a new server
+            let scriptname = "basic-server-3.js"
+            let hostname = ns.purchaseServer("pserv-" + i, ram);
+            ns.scp(scriptname, hostname);
+            let hostRam = ns.getServerMaxRam(hostname);
+            let ram = Math.trunc(hostRam / ns.getScriptRam(scriptname, hostname));
+            ns.exec(scriptname, hostname, ram);
+            ++i;
+            await ns.sleep(60000);
+        }
+    }
+    
+        await ns.sleep(60000);
+    while (i < 25) {
+        // Check if we have enough money to purchase a server
+        if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
+            // If we have enough money, then:
+            //  1. Purchase the server
+            //  2. Copy our hacking script onto the newly-purchased server
+            //  3. Run our hacking script on the newly-purchased server with 3 threads
+            //  4. Increment our iterator to indicate that we've bought a new server
+            let scriptname = "basic-server-4.js"
+            let hostname = ns.purchaseServer("pserv-" + i, ram);
+            ns.scp(scriptname, hostname);
+            let hostRam = ns.getServerMaxRam(hostname);
+            let ram = Math.trunc(hostRam / ns.getScriptRam(scriptname, hostname));
+            ns.exec(scriptname, hostname, ram);
+            ++i;
+            await ns.sleep(60000);
+        }
+    }
+    
+        await ns.sleep(60000);    
 }
