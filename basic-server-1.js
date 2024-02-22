d7ee6bcd83f36966f7ed596dab60426d0aa713de
ARC-Reactor <39143732+ARC-Reactor@users.noreply.github.com>
2024-02-22 13:02:53 -0500

    Create basic-server-1.js

---
+++ basic-server-1.js
@@ -0,0 +1,36 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    const targets = [
+        "aevum-police",
+        "CSEC",
+        "foodnstuff",
+        "sigma-cosmetics",
+        "joesguns",
+        "hong-fang-tea",
+        "harakiri-sushi",
+        "max-hardware",
+        "neo-net",
+        "avmnite-02h",
+        "silver-helix",
+        "computek",
+        "johnson-ortho",
+        "rothman-uni",
+      ]
+
+      while(true) {
+          for (let target of targets){
+            if  (ns.hasRootAccess(target) == true){
+              let moneyThresh = ns.getServerMaxMoney(target);
+              if (moneyThresh != 0){
+                let securityThresh = ns.getServerMinSecurityLevel(target);
+                while (ns.getServerMoneyAvailable(target) < moneyThresh) {
+                    await ns.grow(target);
+                } while (ns.getServerSecurityLevel(target) > securityThresh) {
+                    await ns.weaken(target);
+                } 
+                await ns.hack(target);
+              }
+            }
+            }
+          }
+}
